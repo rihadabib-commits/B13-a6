32904fe2248707bfaf222aa45b2bec37f73615e8
rihadabib-commits <rihadabib@gmail.com>
2026-03-31 22:28:55 +0600

    product added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,59 +1,59 @@
-import React from 'react';
-import Card from '../ui/Card';
+// import React from 'react';
+// import Card from '../ui/Card';
 
-const Product = ({product, setCoin, coin, buy, setBuy}) => {
+// const Product = ({product, setCoin, coin, buy, setBuy}) => {
   
-    return (
-        <div>
-{/* <div className='grid grid-cols-3 space-y-4'>
-{
+//     return (
+//         <div>
+// {/* <div className='grid grid-cols-3 space-y-4'>
+// {
 
-     product.map((product)=>{
-        console.log(product)
-    return        <div className="card  bg-base-100 shadow-sm justify-center flex mx-auto ">
-  <div class="card-body p-6 bg-white rounded-lg shadow-md max-w-md mx-auto">
+//      product.map((product)=>{
+//         console.log(product)
+//     return        <div className="card  bg-base-100 shadow-sm justify-center flex mx-auto ">
+//   <div class="card-body p-6 bg-white rounded-lg shadow-md max-w-md mx-auto">
     
-  <span class="badge b text-white bg-yellow-500 px-2 py-1 rounded-full text-right">{product.tag}</span>
-  <div class="flex flex-col gap-2 mt-4">
-    <h2 class="text-3xl font-bold text-gray-900">{product.name}</h2>
-    <p class="text-sm text-gray-600">{product.description}</p>
-    <span class="text-xl font-semibold text-gray-800 text-bold">${product.price}/<span className='opacity-50'>{product.period}</span></span>
-  </div>
-  <ul class="mt-6 flex flex-col gap-2 text-sm text-gray-700">
-    <li>
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-      </svg>
-Unlimited AI generations
-    </li>
-    <li>
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-      </svg>
-      {product.features}
-    </li>
-    <li>
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-      </svg>
-       {}
-    </li>
-  </ul>
-  <div class="mt-6">
-    <button class="btn px-6 py-2 text-white font-semibold rounded-full bg-gradient-to-r from-blue-600 to-purple-500 hover:from-blue-700 hover:to-purple-600 transition-all">Buy Now</button>
-  </div>
-</div>
-    </div>
-     })}
-</div> */}
+//   <span class="badge b text-white bg-yellow-500 px-2 py-1 rounded-full text-right">{product.tag}</span>
+//   <div class="flex flex-col gap-2 mt-4">
+//     <h2 class="text-3xl font-bold text-gray-900">{product.name}</h2>
+//     <p class="text-sm text-gray-600">{product.description}</p>
+//     <span class="text-xl font-semibold text-gray-800 text-bold">${product.price}/<span className='opacity-50'>{product.period}</span></span>
+//   </div>
+//   <ul class="mt-6 flex flex-col gap-2 text-sm text-gray-700">
+//     <li>
+//       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+//       </svg>
+// Unlimited AI generations
+//     </li>
+//     <li>
+//       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+//       </svg>
+//       {product.features}
+//     </li>
+//     <li>
+//       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline-block text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+//       </svg>
+//        {}
+//     </li>
+//   </ul>
+//   <div class="mt-6">
+//     <button class="btn px-6 py-2 text-white font-semibold rounded-full bg-gradient-to-r from-blue-600 to-purple-500 hover:from-blue-700 hover:to-purple-600 transition-all">Buy Now</button>
+//   </div>
+// </div>
+//     </div>
+//      })}
+// </div> */}
 
-        <Card product={product} setCoin={setCoin} coin={coin} setBuy={setBuy} buy={buy}/>
+//         <Card product={product} setCoin={setCoin} coin={coin} setBuy={setBuy} buy={buy}/>
 
 
       
-  </div>
+//   </div>
         
-    );
-};
+//     );
+// };
 
-export default Product;
+// export default Product;
